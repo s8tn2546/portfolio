2e7f925951d56eb1c5d6545e2e021fb7700eb61b
Ahmed Raza Khan <ahmedrazakhn228@gmail.com>
2025-09-17 14:48:43 +0530

    UI update

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-function updateTerminalVisibility() {
-    const terminalWindow = document.getElementById('terminal-window');
-    const terminalWindowMinimized = document.getElementById('terminal-window-minimized');
-    const isMinimized = terminalWindow.style.visibility === 'hidden';
+const terminalWindow = document.getElementById('terminal-window');
+const terminalIcon = document.getElementById('terminal-icon');
+const day = document.getElementById("day");
+const time = document.getElementById("time");
+const date = document.getElementById("date");
 
-    if (isMinimized) {
+
+function updateTerminalVisibility() {
+
+    if (terminalWindow.style.visibility === 'hidden') {
+
         terminalWindow.style.visibility = 'visible';
-        terminalWindowMinimized.style.visibility = 'hidden';
-    } else {
+        terminalIcon.style.visibility = 'hidden';
+
+    } else if (terminalWindow.style.visibility === 'visible') {
+
         terminalWindow.style.visibility = 'hidden';
-        terminalWindowMinimized.style.visibility = 'visible';
+        terminalIcon.style.visibility = 'visible';
 
     }
 }
 
-function closeTerminal(){
-    const terminalWindow = document.getElementById('terminal-window');
-    const terminalWindowMinimized = document.getElementById('terminal-window-minimized');
-    terminalWindow.style.visibility = 'hidden';
-    terminalWindowMinimized.style.visibility = 'hidden';
+function openTerminal(){
+    terminalIcon.style.visibility = 'hidden';
+    terminalWindow.style.visibility = 'visible';
 }
+
+function closeTerminal(){
+    terminalIcon.style.visibility = 'visible';
+    terminalWindow.style.visibility = 'hidden';
+}
+
+function updateDateTime() {
+    const now = new Date();
+    const config = {hour: '2-digit', minute: '2-digit'};
+    day.textContent = now.toLocaleString('default', { weekday: 'long' });
+    time.textContent = "- " + now.toLocaleTimeString('default', config) + " -";
+    date.textContent = now.toLocaleDateString();
+}
+setInterval(updateDateTime, 1000);
